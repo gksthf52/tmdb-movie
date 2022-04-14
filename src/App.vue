ff23cba01963e8075bfad73124bea7d4a6e9e584
gksthf52 <48721413+gksthf52@users.noreply.github.com>
2022-04-14 18:06:31 +0900

    Add files via upload
    axios 테스트

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,8 @@
       <swiper-slide v-for="a in 6" :key="a">
         <router-link to="/detail">클릭</router-link>
         <div>
-          <img src="http://files.tmdb.org/p/exports/movie_ids_04_28_2017.json.gz" alt="">
+          <!-- <img :src='`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1${imgname}`' alt=""> -->
+          <p>{{imgname.data.results[0].poster_path}}</p>
         </div>
       </swiper-slide>
 
@@ -30,6 +31,8 @@ import axios from 'axios'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
+
 
 export default {
   name: 'App',
@@ -51,18 +54,37 @@ export default {
     };
   },
   data () {
-  return {
-   query: '',
-   results: ''
-  }
- },
+    return {
+      query: '',
+      results: '',
+      imgname : '',
+      // apikey : 'eee59ded3d3f9fb38792c3a4c12362a5'
+    }
+  },
  methods: {
-  getResult() {
-   axios.get('https://api.themoviedb.org/3/search/movie?  api_key=eee59ded3d3f9fb38792c3a4c12362a5').then(response => { this.results = response.data.results })
-     console.log(this.results)
-   }
+   
+ },
+ actions : {
+
  }
 }
+
+axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=en-US&page=1`)
+.then(movie => {
+  console.log(movie)
+  console.log(movie.data.results[0].poster_path)
+  this.imgname = movie
+})
+// const url = "https://api.themoviedb.org/3/movie/popular?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=en-US&page=1";
+
+
+// axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1${imgname}`)
+// .then(movie => {
+//   console.log(movie)
+//   console.log(movie.data.results[0].poster_path)
+//   this.imgname = movie.data.results[0].poster_path
+// })
+
 </script>
 
 <style>
@@ -75,8 +97,9 @@ export default {
   margin-top: 60px;
 }
 
-.movie , .swiper-slide{
+.swiper-slide{
   height: 150px;
-  /* background: #000; */
+  border: 1px solid #000;
+  box-sizing: border-box;
 }
 </style>
