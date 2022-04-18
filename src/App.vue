986482e0dfb83ae8140d49e7186ea8f0abcc678a
gksthf52 <48721413+gksthf52@users.noreply.github.com>
2022-04-19 00:56:40 +0900

    Add files via upload
    개봉예정 추가

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,25 @@
   <router-link to="/">홈</router-link>
   <router-link to="/detail">상세페이지</router-link>
     <swiper
+    :breakpoints="swiperOptions.breakpoints"
     :modules="modules"
-    :slides-per-view="6"
+    :slides-per-view="8"
     navigation
     :pagination="{ clickable: true }"
     @swiper="onSwiper"
     @slideChange="onSlideChange">
 
       <swiper-slide v-for='a in poster' :key='a'>
-        <!-- <router-link to="/detail"></router-link> -->
+        <router-link to="/detail">
+          <!-- <img class="poster" :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="">         -->
+        </router-link>
         <div class="poster">          
-          <img class="poster-img" :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="">
+          <div class="poster-img">
+            <img :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="" style="width:100%">
+          </div>
           <div class="poster-con">
             <h5>{{a.title}}</h5>     
-            <span>{{a.release_date}}</span>       
+            <span>{{a.release_date}}</span>         
           </div>
           <!-- <img :src='imgurl' alt=""> -->
           <!-- <img :src='`https://www.themoviedb.org/t/p/w220_and_h330_face/${imgurl}`' alt=""> -->
@@ -23,10 +28,10 @@
       </swiper-slide>
     </swiper>
 
-<!--  -->
     <swiper
+    :breakpoints="swiperOptions.breakpoints"
     :modules="modules"
-    :slides-per-view="6"
+    :slides-per-view="8"
     navigation
     :pagination="{ clickable: true }"
     @swiper="onSwiper"
@@ -37,7 +42,36 @@
           <!-- <img class="poster" :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="">         -->
         </router-link>
         <div class="poster">          
-          <img class="poster-img" :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="">
+          <div class="poster-img">
+            <img :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="" style="width:100%">
+          </div>
+          <div class="poster-con">
+            <h5>{{a.title}}</h5>     
+            <span>{{a.release_date}}</span>         
+          </div>
+          <!-- <img :src='imgurl' alt=""> -->
+          <!-- <img :src='`https://www.themoviedb.org/t/p/w220_and_h330_face/${imgurl}`' alt=""> -->
+        </div>
+      </swiper-slide>
+    </swiper>
+
+    <swiper
+    :breakpoints="swiperOptions.breakpoints"
+    :modules="modules"
+    :slides-per-view="8"
+    navigation
+    :pagination="{ clickable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange">
+
+      <swiper-slide v-for='a in poster2' :key='a'>
+        <router-link to="/detail">
+          <!-- <img class="poster" :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="">         -->
+        </router-link>
+        <div class="poster">          
+          <div class="poster-img">
+            <img :src='`https://www.themoviedb.org/t/p/w200${a.poster_path}`' alt="" style="width:100%">
+          </div>
           <div class="poster-con">
             <h5>{{a.title}}</h5>     
             <span>{{a.release_date}}</span>         
@@ -87,22 +121,43 @@ export default {
       apikey : '',
       imgurl : '',
       poster : [],
-      poster1 : []
+      poster1 : [],
+      poster2 : [],
+      swiperOptions: {
+          breakpoints: {
+            1024: {
+            slidesPerView: 7,
+            // spaceBetween: 10
+            },
+            680: {
+            slidesPerView: 5,
+            },
+            320: {
+            slidesPerView: 3,
+            }
+          }
+      }   
     }
   },
   mounted () {
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=en-US&page=1`)
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=ko&page=1`)
      .then( 결과 =>{
        //  this.imgurl = 결과.data
       // this.apikey = 'eee59ded3d3f9fb38792c3a4c12362a5' 634649
       this.poster = 결과.data.results
        console.log(this.poster);
      })
-    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=en-US&page=1`)
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=ko&page=1`)
      .then( 결과1 =>{
       this.poster1 = 결과1.data.results
        console.log(this.poster1);
      })
+    axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=eee59ded3d3f9fb38792c3a4c12362a5&language=ko&page=1`)
+     .then( 결과2 =>{
+      this.poster2 = 결과2.data.results
+       console.log(this.poster2);
+     })
+     
   },
  methods: {
    
@@ -140,15 +195,15 @@ export default {
 }
 
 .poster{
-  margin-right: 15px;
-  border-radius: 10px;
-  background: #fff;
-  box-shadow: 0px 0px 9px #aaa;
-  overflow: hidden;
+  margin-right: 20px;
+  /* border-radius: 10px;
+  box-shadow: 0px 0px 5px #94949466;
+  overflow: hidden; */
+  color: #fff;
 }
 
 .poster-img{
-  width: 100%;
+  height: 230px;
 }
 
 .poster-con{
@@ -164,6 +219,7 @@ export default {
 
 body{
   margin: 0;
+  background: #141414;
 }
 
 :root{
@@ -171,7 +227,7 @@ body{
 }
 
 h5{
-  margin: 0;
+  margin: 0 0 5px 0;
   font-size: 12px;
 }
 </style>
